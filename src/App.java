c90436fa1e543d02d63564171729f43a9cf0fb6f
TonyNavas <tonynavas777@gmail.com>
2021-10-22 11:19:17 -0600

    Agregando la estructura para el cliente

--- src/App.java
+++ src/App.java
@@ -1,4 +1,17 @@
 public class App
 {
-
+    public static void main(String[] args) {
+        Pizza pizza;
+        Cocinero cocinero = new Cocinero();
+        PizzaBuilder bpb = new BarbacoaPizzaBuilder();
+        PizzaBuilder cpb = new CarbonaraPizzaBuilder();
+        cocinero.setPizzaBuilder(bpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
+        cocinero.setPizzaBuilder(cpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
+    }
 }
