8045d12e17c84b9f6b34ea8455a444638cc553c0
TonyNavas <tonynavas777@gmail.com>
2021-10-22 11:15:15 -0600

    Agregando la clase BarbacoaPizzaBuilder

---
+++ src/BarbacoaPizzaBuilder.java
@@ -0,0 +1,18 @@
+public class BarbacoaPizzaBuilder extends PizzaBuilder {
+    @Override
+    public void ponerNombre() {
+        pizza.setNombre("Barbacoa");
+    }
+    @Override
+    public void crearMasa() {
+        pizza.setMasa("gorda");
+    }
+    @Override
+    public void crearSalsa() {
+        pizza.setSalsa("barbacoa");
+    }
+    @Override
+    public void crearIngredientes() {
+        pizza.setIngredientes("mozzarella, ternera, cebolla, maíz");
+    }
+}
