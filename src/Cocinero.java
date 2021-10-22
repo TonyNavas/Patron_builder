e7852547ca080c206cffc18a5c791115bb724a49
TonyNavas <tonynavas777@gmail.com>
2021-10-22 11:16:52 -0600

    Agregando la clase Cocinero

---
+++ src/Cocinero.java
@@ -0,0 +1,22 @@
+public class Cocinero {
+    private PizzaBuilder pizzaBuilder;
+    public void setPizzaBuilder(PizzaBuilder pb)
+    {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza()
+    {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void crearPizza()
+    {
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngredientes();
+    }
+
+}
